ba3eb3bb3077eb50812b95021dfda0c07707259a
Vincent <Frostlight@users.noreply.github.com>
2017-11-25 23:41:30 -0500

    indeed.x

--- IndeedHideArchivedJobs.js
+++ IndeedHideArchivedJobs.js
@@ -25,7 +25,7 @@
 // @ids			 IndeedHideArchivedJobs
 // @name         Indeed Hide Archived Jobs
 // @namespace    https://github.com/frostlight
-// @version      1.2
+// @version      1.6
 // @author       Frostlight
 // @description  Removes archived jobs from Indeed searches
 // @license      MIT; https://spdx.org/licenses/MIT.html
@@ -33,8 +33,8 @@
 // @icon		 https://raw.githubusercontent.com/Frostlight/Userscripts-Indeed/master/icon.png
 // @domain 		 raw.github.com
 // @domain		 indeed.com
-// @include      https://*.indeed.com/*
-// @include      https://indeed.com/*
+// @include      https://*.indeed.*/*
+// @include      https://indeed.*/*
 // @grant        none
 // ==/UserScript==
 
--- IndeedHideSavedJobs.js
+++ IndeedHideSavedJobs.js
@@ -25,7 +25,7 @@
 // @ids			 IndeedHideSavedJobs
 // @name         Indeed Hide Saved Jobs
 // @namespace    https://github.com/frostlight
-// @version      1.1
+// @version      1.6
 // @author       Frostlight
 // @description  Removes saved jobs from Indeed searches
 // @license      MIT; https://spdx.org/licenses/MIT.html
@@ -33,8 +33,8 @@
 // @icon		 https://raw.githubusercontent.com/Frostlight/Userscripts-Indeed/master/icon.png
 // @domain 		 raw.github.com
 // @domain		 indeed.com
-// @include      https://*.indeed.com/*
-// @include      https://indeed.com/*
+// @include      https://*.indeed.*/*
+// @include      https://indeed.*/*
 // @grant        none
 // ==/UserScript==
 
